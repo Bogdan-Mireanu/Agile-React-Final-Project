5caf6bbe1520c88c77ca7d0796cbce9ea0a47ec8
Timea Ferencz <timea.ferencz93@yahoo.com>
2020-10-10 21:03:32 +0300

    add router navigation, modal for notes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import BookDetails from './components/BookDetails/BookDetails';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+    
     <div className="App">
         <div className="navbar">
           Navbar...
         </div>
         <Header/>
-        <SearchBar/>
+        <Switch>
+          <Route exact path="/bookDetails/:id"> <BookDetails /></Route>
+          <Route exact path="/search">  <SearchBar /></Route>
+        </Switch>
+     
+      
     </div>
+    </Router>
   );
 }
 
--- src/components/BookDetails/BookDetails.js
+++ src/components/BookDetails/BookDetails.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Button, Modal } from 'react-bootstrap';
 import CustomModal from '../Modal/CustomModal';
+import {useParams} from 'react-router-dom';
+import Note from '../Note/Note';
 
 export default function BookDetails(prop) {
-   
+    const [modal, showModal] = React.useState(false);
     const [data, setData] = useState({});
-    const id = prop.id;
-    
+    let { id } = useParams();
     useEffect(() => {
         async function getDetails(){
         const result = await axios.get(
@@ -24,7 +26,15 @@ export default function BookDetails(prop) {
                 <h2> {data.volumeInfo.title}</h2>
                 <h5 className='text-muted'>Publisher: {data.volumeInfo.publisher}</h5>
                 <h5 className='text-muted'>Publish Date: {data.volumeInfo.publishedDate}</h5>
-                {data.volumeInfo.description}
+                <h5 className='text-muted'>Description: {data.volumeInfo.description}</h5>
+                    <img className="" src={data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : null} alt="cover" />
+                <Button onClick={()=>showModal(!modal)}>Add note</Button>
+                {
+                modal && 
+                <CustomModal toggleModal={showModal} show={modal}>
+                <Note></Note>
+                </CustomModal>
+                }
             </div>
             )}
         </>
